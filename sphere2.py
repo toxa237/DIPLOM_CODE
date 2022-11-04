396880559efed97f05cd3ff833f0a5e527929db7
toxa237 <qwetyiop57@gmail.com>
2022-11-04 14:21:38 +0200

    виправив деякі помилки, але все ж результат фігня

--- sphere2.py
+++ sphere2.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
@@ -37,26 +38,26 @@ def coef_a_b(l):
          jv(l + 0.5, Eps_2 * K * R)),
         ((n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * R),
          (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * R),
-         jv(l + 0.5, Eps_2 * K * R))
+         jvp(l + 0.5, Eps_2 * K * R))
     ))
     a, b = solve_linear_system(matrix, X, Y).values()
     return float(a), float(b)
 
 
-# def coef_a_b(l):
-#     J_1 = jv(l+0.5, Eps_1*K*R)
-#     Jp_1 = jvp(l+0.5, Eps_1*K*R)
-#     J_2 = jv(l+0.5, Eps_2*K*R)
-#     Jp_2 = jvp(l + 0.5, Eps_2 * K * R)
-#     N = yve(l + 0.5, Eps_1 * K * R)
-#     Np = yve(l + 0.5, Eps_1 * K * R)
-#     b = (Jp_2-(n1/n2)*J_2*(Jp_1/J_1))/(Np-(n1/n2)*N*(Jp_1/J_1))
-#     a = (J_2-N*b)/(((Eps_2/Eps_1)**0.25) * J_1)
-#     return a, b
+def coef_a_b2(l):
+    a = (Eps_2 / Eps_1) ** 0.25 * jv(l + 0.5, Eps_1 * K * R)
+    b = (Eps_2 / Eps_1) ** 0.25 * yve(l + 0.5, Eps_1 * K * R)
+    c = jv(l + 0.5, Eps_2 * K * R)
+    ap = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * R)
+    bp = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * R)
+    cp = jvp(l + 0.5, Eps_2 * K * R)
+    A = (c * bp - b * cp) / (bp * a - ap * b)
+    B = (a * cp - ap * c) / (bp * a - ap * b)
+    return A, B
 
 
 def val_field(x, z):
-    global R, K, Eps_1, Eps_2, coon, A_kml, B_kml, L
+    global R, K, Eps_1, Eps_2, A_kml, B_kml, L
     val = 0
     r, theta, phi = system_cords_sphere(x, z)
     r = r * 1e-9
@@ -65,41 +66,53 @@ def val_field(x, z):
     if r > R:
         for l in L:
             for m in range(-l, l + 1):
-                val += (1 / ((K * r) ** 0.5 * Eps_2 ** 0.25)) * jv(l + 0.5, Eps_2 * K * r) * sph_harm(m, l, phi,
-                                                                                                      theta)
+                val += (1 / (((K * r) ** 0.5) * (Eps_2 ** 0.25))) * jv(l + 0.5, Eps_2 * K * r) * \
+                       sph_harm(m, l, phi, theta)
     else:
         for l in L:
             for m in range(-l, l + 1):
-                val += (1 / ((K * r) ** 0.5 * Eps_1 ** 0.25)) * (
-                        A_kml[l] * jv(l + 0.5, Eps_1 * K * r) + B_kml[l] * yve(l + 0.5, Eps_1 * K * r)) * sph_harm(m, l, phi,
-                                                                                                     theta)
+                val += (1 / ((K * r) ** 0.5 * Eps_1 ** 0.25)) * \
+                       (A_kml[l] * jv(l + 0.5, Eps_1 * K * r) + B_kml[l] * yve(l + 0.5, Eps_1 * K * r)) * \
+                       sph_harm(m, l, phi, theta)
     return np.complex128(val).real
 
 
 if __name__ == '__main__':
     # coon = create_engine('mysql+pymysql://toxa:password@localhost:3306/DIPLOM', echo=False)
     num_of_points = 400
-    xy = np.linspace(-1500, 1500, num=num_of_points)
+    xy = np.linspace(-20, 20, num=num_of_points)
     R = 10e-6
+    R2 = 10
     Eps_2 = 1
     Eps_1 = 0.004
     n2 = 1
     n1 = np.sqrt(Eps_1)
     K = 2 * np.pi / 800e-9
-
     L = range(2)
-    A_kml, B_kml = np.array([coef_a_b(l) for l in range(3)]).T
+
+    A_kml, B_kml = np.array([coef_a_b2(l) for l in range(3)]).T
+    # A_kml2, B_kml2 = np.array([coef_a_b(l) for l in range(3)]).T
+    # print(A_kml, A_kml2, B_kml, B_kml2, sep='\n')
+    # exit()
 
     FIELD = []
     for q, i in enumerate(tqdm.tqdm(xy)):
         FIELD.append([])
         for j in xy:
             FIELD[q].append(val_field(i, j))
+    FIELD = np.array(FIELD)
 
-    # joblib.dump(FIELD, 'data/FIELD.pkl')
-    # FIELD = joblib.load('data/FIELD.pkl')
+    # x1, y1 = np.meshgrid(xy, xy)
+    # plt.contourf(x1, y1, FIELD)
+    # plt.show()
 
     fig = go.Figure(data=go.Contour(
-        z=FIELD
-    ))
+            z=FIELD, x=xy, y=xy
+        ))
+
+    fig.add_shape(type="circle",
+                  xref="x", yref="y",
+                  x0=-R2, y0=-R2, x1=R2, y1=R2,
+                  line_color="LightSeaGreen",
+                  )
     fig.show()
