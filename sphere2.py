2df797266adfcb6f136c4e3ec74ba85e7eeaeeff
toxa237 <qwetyiop57@gmail.com>
2022-11-12 11:12:52 +0200

    фігня

--- sphere2.py
+++ sphere2.py
@@ -27,52 +27,54 @@ def system_cords_sphere(x, z):
         phi = 0
     else:
         phi = np.pi
-    return r, theta, phi
+    return r * STEP, theta, phi
 
 
 def coef_a_b(l):
     global Eps_1, Eps_2, K, R, n1, n2
+    r = R * STEP
     matrix = Matrix((
-        ((Eps_2 / Eps_1) ** 0.25 * jv(l + 0.5, Eps_1 * K * R),
-         (Eps_2 / Eps_1) ** 0.25 * yve(l + 0.5, Eps_1 * K * R),
-         jv(l + 0.5, Eps_2 * K * R)),
-        ((n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * R),
-         (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * R),
-         jvp(l + 0.5, Eps_2 * K * R))
+        ((Eps_2 / Eps_1) ** 0.25 * jv(l + 0.5, Eps_1 * K * r),
+         (Eps_2 / Eps_1) ** 0.25 * yve(l + 0.5, Eps_1 * K * r),
+         jv(l + 0.5, Eps_2 * K * r)),
+        ((n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * r),
+         (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * r),
+         jvp(l + 0.5, Eps_2 * K * r))
     ))
     a, b = solve_linear_system(matrix, X, Y).values()
-    return float(a), float(b)
+    return a, b
 
 
 def coef_a_b2(l):
-    a = (Eps_2 / Eps_1) ** 0.25 * jv(l + 0.5, Eps_1 * K * R)
-    b = (Eps_2 / Eps_1) ** 0.25 * yve(l + 0.5, Eps_1 * K * R)
-    c = jv(l + 0.5, Eps_2 * K * R)
-    ap = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * R)
-    bp = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * R)
-    cp = jvp(l + 0.5, Eps_2 * K * R)
-    A = (c * bp - b * cp) / (bp * a - ap * b)
-    B = (a * cp - ap * c) / (bp * a - ap * b)
+    global Eps_1, Eps_2, K, R, n1, n2, A_kml1
+    r = R*STEP
+    a = (Eps_2 / Eps_1) ** 0.25 * jv(l + 0.5, Eps_1 * K * r)
+    b = (Eps_2 / Eps_1) ** 0.25 * yve(l + 0.5, Eps_1 * K * r)
+    c = jv(l + 0.5, Eps_2 * K * r)
+    ap = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * jvp(l + 0.5, Eps_1 * K * r)
+    bp = (n1 / n2) * (Eps_2 / Eps_1) ** 0.25 * yvp(l + 0.5, Eps_1 * K * r)
+    cp = jvp(l + 0.5, Eps_2 * K * r)
+    A = ((c * bp - b * cp) / (bp * a - ap * b))/A_kml1
+    B = ((a * cp - ap * c) / (bp * a - ap * b))/A_kml1
     return A, B
 
 
 def val_field(x, z):
-    global R, K, Eps_1, Eps_2, A_kml, B_kml, L
+    global R, K, Eps_1, Eps_2, A_kml1, A_kml2, B_kml2, L
     val = 0
     r, theta, phi = system_cords_sphere(x, z)
-    r = r * 1e-9
     if r == 0:
         return 0
     if r > R:
-        for l in L:
+        for i, l in enumerate(L):
             for m in range(-l, l + 1):
-                val += (1 / (((K * r) ** 0.5) * (Eps_2 ** 0.25))) * jv(l + 0.5, Eps_2 * K * r) * \
+                val += (1 / (((K * r) ** 0.5) * (Eps_2 ** 0.25))) * A_kml1 * jv(l + 0.5, Eps_2 * K * r) * \
                        sph_harm(m, l, phi, theta)
     else:
-        for l in L:
+        for i, l in enumerate(L):
             for m in range(-l, l + 1):
                 val += (1 / ((K * r) ** 0.5 * Eps_1 ** 0.25)) * \
-                       (A_kml[l] * jv(l + 0.5, Eps_1 * K * r) + B_kml[l] * yve(l + 0.5, Eps_1 * K * r)) * \
+                       (A_kml2[i] * jv(l + 0.5, Eps_1 * K * r) + B_kml2[i] * yve(l + 0.5, Eps_1 * K * r)) * \
                        sph_harm(m, l, phi, theta)
     return np.complex128(val).real
 
@@ -80,18 +82,19 @@ def val_field(x, z):
 if __name__ == '__main__':
     # coon = create_engine('mysql+pymysql://toxa:password@localhost:3306/DIPLOM', echo=False)
     num_of_points = 400
-    xy = np.linspace(-20, 20, num=num_of_points)
-    R = 10e-6
-    R2 = 10
+    xy = np.linspace(-100, 100, num=num_of_points)
+    R = 10
+    STEP = 10e-3
     Eps_2 = 1
     Eps_1 = 0.004
     n2 = 1
     n1 = np.sqrt(Eps_1)
-    K = 2 * np.pi / 800e-9
-    L = range(2)
+    K = 2 * np.pi / (400 * 10e-9)
+    L = range(3)
+    A_kml1 = 1
 
-    A_kml, B_kml = np.array([coef_a_b2(l) for l in range(3)]).T
-    # A_kml2, B_kml2 = np.array([coef_a_b(l) for l in range(3)]).T
+    A_kml2, B_kml2 = np.array([coef_a_b2(l) for l in L]).T
+    # A_kml, B_kml = np.array([coef_a_b(l) for l in range(3)]).T
     # print(A_kml, A_kml2, B_kml, B_kml2, sep='\n')
     # exit()
 
@@ -102,17 +105,20 @@ if __name__ == '__main__':
             FIELD[q].append(val_field(i, j))
     FIELD = np.array(FIELD)
 
+    print(np.max(FIELD), np.min(FIELD))
+
     # x1, y1 = np.meshgrid(xy, xy)
-    # plt.contourf(x1, y1, FIELD)
+    # plt.contourf(x1, y1, FIELD, cmap='hsv')
     # plt.show()
+    # exit()
 
     fig = go.Figure(data=go.Contour(
-            z=FIELD, x=xy, y=xy
-        ))
+        z=FIELD, x=xy, y=xy
+    ))
 
     fig.add_shape(type="circle",
                   xref="x", yref="y",
-                  x0=-R2, y0=-R2, x1=R2, y1=R2,
+                  x0=-R, y0=-R, x1=R, y1=R,
                   line_color="LightSeaGreen",
                   )
     fig.show()
